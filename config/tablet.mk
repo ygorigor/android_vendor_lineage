c0099899091e5ca919d81a74c8db87c717883de9
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-11-05 02:22:01 +0800

    config: Allow Setup Wizard rotation
    Change-Id: Ifc769dbac874528d43830a4cfee40c95c4c48719

--- config/tablet.mk
+++ config/tablet.mk
@@ -4,5 +4,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/large_screen_common.mk)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.freeform_window_management.xml
 
+# Setup Wizard
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.setupwizard.rotation_locked=false
+
 # Tablet-specific overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/tablet
