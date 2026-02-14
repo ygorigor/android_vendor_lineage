2be51e09b5654b220290fb6613203189b2c5bda9
someone5678 <someone5678@users.noreply.github.com>
2026-02-14 10:42:44 +0800

    config: Set default wifi country code
    * To 00
    * Make it overridable
    
    Change-Id: I2025fc927363baf547b36ecd6ebe9c7fbc4d7611

--- config/common.mk
+++ config/common.mk
@@ -352,3 +352,7 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 # Disable default frame rate limit for games
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.graphics.game_default_frame_rate.disabled=true
+
+# Default wifi country code
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.boot.wificountrycode?=00
